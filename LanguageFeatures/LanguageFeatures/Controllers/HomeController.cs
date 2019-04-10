6e54f902cba2f7dccdea7e90de8ae928d9b7bd7d
Niklas <niklas@develo.no>
2019-04-10 09:54:44 +0200

    ryddet opp kode i HomeCOntroller

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -12,14 +12,9 @@ namespace LanguageFeatures.Controllers
         public ViewResult Index()
         {
             List<string> results = new List<string>();
-            foreach (Product p in Product.GetProducts())
-            {
-                string name = p?.Name ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
-            }
-            return View(results);
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
         }
     }
 }
