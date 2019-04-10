b82171638c1149276473d4137eb86d0a4d0fc120
Niklas <niklas@develo.no>
2019-04-10 09:13:01 +0200

    la til in stock

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,16 +7,24 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
-        
+        public bool InStock { get; }
         public static Product[] GetProducts()
         {
 
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
             Product lifejacket = new Product
