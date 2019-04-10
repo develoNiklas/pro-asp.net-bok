98fa01165bbab4dff56fe95ab0b508e071002034
Niklas <niklas@develo.no>
2019-04-10 09:00:54 +0200

    Added rerlated to home controller

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace LanguageFeatures.Controllers
             {
                 string name = p?.Name;
                 decimal? price = p?.Price;
+                string relatedName = p?.Related?.Name;
                 results.Add(string.Format("Name: {0}, Price: {1}", name, price));
             }
             return View(results);
