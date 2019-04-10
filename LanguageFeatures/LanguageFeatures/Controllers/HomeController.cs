9c0c207a9dcc927c9b819a2791492dd00848f83a
Niklas <niklas@develo.no>
2019-04-10 09:03:21 +0200

    la til fallback values i home controller

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -14,9 +14,9 @@ namespace LanguageFeatures.Controllers
             List<string> results = new List<string>();
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                string relatedName = p?.Related?.Name;
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
                 results.Add(string.Format("Name: {0}, Price: {1}", name, price));
             }
             return View(results);
