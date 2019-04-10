45add8b55d61a48c9c24d7e6f2a3729440fa6cae
Niklas <niklas@develo.no>
2019-04-10 08:47:38 +0200

    kapttel 2 og starten på kap 4

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+            }
+            return View(results);
+        }
+    }
+}
