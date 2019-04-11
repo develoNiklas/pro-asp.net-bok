b56635ab77bd1da65e9fc70f6233a8fc30bdd4d6
Niklas <niklas@develo.no>
2019-04-11 08:53:19 +0200

    la til fake reposiutory

--- SportStore/SportStore/Startup.cs
+++ SportStore/SportStore/Startup.cs
@@ -6,13 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using SportStore.Models;
 namespace SportStore
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
