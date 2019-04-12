d323b220d2d3cfa186bc29a04c6fb1941d58c4b4
Niklas <niklas@develo.no>
2019-04-12 07:48:08 +0200

    la til kategori navbar

---
+++ SportsStore/SportsStore/Models/ViewModels/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
